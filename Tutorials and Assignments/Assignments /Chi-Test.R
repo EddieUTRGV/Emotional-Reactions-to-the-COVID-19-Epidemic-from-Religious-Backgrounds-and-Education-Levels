84a4fb0bc1f1ba35f0c17987d8b3375b8c5f4e13
EddieUTRGV <eduardo.saldana02@utrgv.edu>
2025-04-09 21:31:18 +0000

    Chi-Square Test Assignment

--- Tutorials and Assignments/Assignments /Chi-Test.R
+++ Tutorials and Assignments/Assignments /Chi-Test.R
@@ -1,41 +1,22 @@
-# Load necessary libraries
+
 library(dplyr)
 library(ggplot2)
 
-# Load the starwars dataset
+#The Chi-Square test will be used to determine if there is an association
+#between the count of characters and frequencies of gender.
+
 data("starwars")
 
-# Step 2: Summarize Your Categorical Variable
-sex_summary <- starwars %>%
-  group_by(sex) %>%
-  summarize(count = n())
+table(starwars$sex)
 
-hair_color_summary <- starwars %>%
-  group_by(hair_color) %>%
-  summarize(count = n())
+chisq.test(table(starwars$sex))$expected
 
-# Step 3: Check Assumptions (not coded here, print expected counts if needed)
+chisq.test(table(starwars$sex))
 
-# Step 4: Run the Chi-Squared Goodness-of-Fit Test
-# For Sex
-sex_table <- table(starwars$sex)
-chi_squared_sex <- chisq.test(sex_table)
+ggplot(starwars, aes(x = sex)) +
+  geom_bar() +
+  labs(title = "Observed Frequencies", x = "Category", y = "Count")
 
-# For Hair Color
-hair_color_table <- table(starwars$hair_color)
-chi_squared_hair_color <- chisq.test(hair_color_table)
-
-# Step 5: Visualize the Distribution
-# Bar Plot for Sex
-ggplot(sex_summary, aes(x = sex, y = count)) +
-  geom_bar(stat = "identity") +
-  labs(title = "Distribution of Sex in Star Wars", x = "Sex", y = "Count")
-
-# Bar Plot for Hair Color
-ggplot(hair_color_summary, aes(x = hair_color, y = count)) +
-  geom_bar(stat = "identity") +
-  labs(title = "Distribution of Hair Color in Star Wars", x = "Hair Color", y = "Count")
-
-# Step 6: Print chi-squared test results
-print(chi_squared_sex)
-print(chi_squared_hair_color)
+#The Chi-Square test showed us that there is a significant p-value of 2.2e-16.
+#This means that the amount of genders in the Star Wars universe is
+#significantly different and not equal at all.
