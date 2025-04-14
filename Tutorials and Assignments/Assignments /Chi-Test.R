bf801f7acde39cd7e4acd54c7f91a6e019c30873
EddieUTRGV <eduardo.saldana02@utrgv.edu>
2025-04-14 16:57:06 +0000

    T-Test

--- Tutorials and Assignments/Assignments /Chi-Test.R
+++ Tutorials and Assignments/Assignments /Chi-Test.R
@@ -2,8 +2,14 @@
 library(dplyr)
 library(ggplot2)
 
-#The Chi-Square test will be used to determine if there is an association
+#The Chi-Square test will be used to determine if there is an association 
 #between the count of characters and frequencies of gender.
+
+#Null Hypothesis- There is no proportion of difference in gender within the 
+#sample for starwars.
+
+#Alternative Hypothesis- There is a proportion of difference in gender within
+#the starwars sample.
 
 data("starwars")
 
@@ -19,4 +25,5 @@ ggplot(starwars, aes(x = sex)) +
 
 #The Chi-Square test showed us that there is a significant p-value of 2.2e-16.
 #This means that the amount of genders in the Star Wars universe is
-#significantly different and not equal at all.
+#significantly different and not equal at all. 
+#This reinforces our Alternative Hypothesis.
---
+++ Tutorials and Assignments/Assignments /T-Test.R
@@ -0,0 +1,18 @@
+library(dplyr)
+library(ggplot2)
+
+
+data("starwars")
+head(starwars)
+
+t.test(starwars$height, mu = 145)
+
+ggplot(starwars, aes(x = height)) +
+  geom_histogram(binwidth = 5, fill = "blue", alpha = 0.7, color = "black") +
+  geom_vline(xintercept = 145, linetype = "dashed", color = "red") +
+  labs(title = "Distribution of Star Wars Character Heights",
+       x = "Height (cm)",
+       y = "Frequency") +
+  theme_minimal() +
+  annotate("text", x = 145, y = 10, label = "Population Mean (75)", color = "red", vjust = -1)
+
